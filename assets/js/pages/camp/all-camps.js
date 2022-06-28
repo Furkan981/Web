53a3f0f60048e16d272ada98dfdc51f35c2abf24
Arbnor <arbnoruka@gmail.com>
2022-06-28 02:08:43 +0200

    Homepage/Filtering/Navigation

--- assets/js/pages/camp/all-camps.js
+++ assets/js/pages/camp/all-camps.js
@@ -1,9 +1,9 @@
 function findCards() {
 
-
 const userCardTemplate = document.querySelector("[data-user-template]")
 const userCardContainer = document.querySelector("[data-user-cards-container]")
 const searchInput = document.querySelector("[data-search]")
+
 
 let users = []
 
@@ -12,11 +12,11 @@ searchInput.addEventListener("input", e => {
   users.forEach(user => {
     const isVisible =
       user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
+      user.description.toLowerCase().includes(value)
     user.element.classList.toggle("hide", !isVisible)
   })
 })
-
+/*
 fetch("https://jsonplaceholder.typicode.com/users")
   .then(res => res.json())
   .then(data => {
@@ -30,24 +30,38 @@ fetch("https://jsonplaceholder.typicode.com/users")
       return { name: user.name, email: user.email, element: card }
     })
   })
-  
+ */ 
   //camps = await Camp.find({}).fetch().then()
- /* fetch("/test")
+  fetch("/test")
   .then(res => res.json())
   .then(data => {
     users = data.map(user => {
       const card = userCardTemplate.content.cloneNode(true).children[0]
-      const header = card.querySelector("[data-header]")
-      const body = card.querySelector("[data-body]")
-      header.textContent = user.name
+      const title = card.querySelector("[data-title]")
+      const price = card.querySelector("[data-price]")
+      const body = card.querySelector("[data-spots]")
+      const link = card.querySelector("[data-link]")
+      title.textContent = user.name +" " + user.id
+      price.textContent = user.price
       body.textContent = user.description
+      card.querySelector('#action-link').href += '/' + user.id;
+      link.append(' '+user.id)
+      //link.addEventListener("click", urlWeiterleitung(user.id));
       userCardContainer.append(card)
-      return { name: user.name, email: user.description, element: card }
+      return { name: user.name, price: user.price,description: user.description, id:user.id, ress:user, element: card }
     })
   })
-  */
   
   
+   
 
 
 }
+
+
+
+function urlWeiterleitung(id) {
+  var url = location.hostname;
+  location.href.replace(url+"/camp/"+id)
+}
+
