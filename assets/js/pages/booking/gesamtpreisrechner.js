887656f1db652aa43cd4da784917a04d97e66c12
Arbnor <arbnoruka@gmail.com>
2022-06-29 21:17:35 +0200

    confirm ü meta tags

--- assets/js/pages/booking/gesamtpreisrechner.js
+++ assets/js/pages/booking/gesamtpreisrechner.js
@@ -9,4 +9,22 @@ function calculate2(von, bis, preis) {
     } else {
         return gesamtpreis;
     }
+
 }
+
+
+$(function() {
+    $('#total').on("click", function () {
+      $.ajax({
+        type: 'GET',
+        url: '/orders',
+        success: function(order) {
+          var html = '';
+          for (var i = 0; i< order.length; i++) {
+              html += '<h2>' + order[i].name + ' ' + order[i].drink + '</h2>';
+          }
+          $('#target').html(html);
+        }
+      });
+    });
+  });
